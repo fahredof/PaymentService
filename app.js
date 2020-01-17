fab7d5de7ef795d9053a2041fac1b0012b636768
fahred <turik20121998@gmail.com>
2020-01-17 15:00:10 +0300

    config eureka

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const Eureka = require('eureka-js-client').Eureka;
 
 const app = express();
 
@@ -21,6 +22,36 @@ async function start() {
         });
         app.use(bodyParser.json());
         app.use("/payment", require("./routes/index"));
+
+        const clientEureca = new Eureka({
+
+            instance: {
+                app: 'Payment',
+                ipAddr: '127.0.0.1',
+                vipAddress: 'Payment',
+                dataCenterInfo: {
+                    name: 'MyOwn',
+            },
+
+            port: 8082,
+
+        },
+            eureca: {
+                 serviceUrls:{
+                     default: [
+                         'http://localhost:8085/eureka'
+                     ]
+                 },
+                host: 'http://localhost:',
+                    port: 8085,
+                    servicePath: 'eureca'
+            },
+        });
+
+        clientEureca.start(error => {
+            console.log(error || 'eureca started');
+        });
+
     } catch (error) {
         logger.info(error);
     }
